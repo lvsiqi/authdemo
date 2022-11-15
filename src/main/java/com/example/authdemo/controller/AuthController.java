9ff7fbf2ce2dc5614e14ddd7b55cbd0580df34a1
lvsiqi <siqi.lv@kikatech.com>
2022-11-15 22:44:49 +0800

    Initial commit

---
+++ src/main/java/com/example/authdemo/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.authdemo.controller;
+
+import com.xkcoding.justauth.AuthRequestFactory;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthStateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/oauth")
+public class AuthController {
+    @Autowired
+    private AuthRequestFactory factory;
+    @GetMapping
+    public List<String> list() {
+        return factory.oauthList();
+    }
+
+    @GetMapping("/login/{type}")
+    public void login(@PathVariable String type, HttpServletResponse response) throws IOException {
+        AuthRequest authRequest = factory.get(type);
+        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+    }
+
+    @GetMapping("/{type}/callback")
+    public AuthResponse login(@PathVariable String type, AuthCallback callback) {
+        AuthRequest authRequest = factory.get(type);
+        AuthResponse response = authRequest.login(callback);
+        return response;
+    }
+}
